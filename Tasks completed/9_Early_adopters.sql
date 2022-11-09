c6acc892afbc274b31c26d9ff0c296c3c7e1721f
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2022-11-09 10:39:29 -0500

    Added extra task
    Also created folder to organize saved searches

---
+++ Tasks completed/9_Early_adopters.sql
@@ -0,0 +1,21 @@
+--
+-- File generated with SQLiteStudio v3.3.3 on Wed Nov 9 10:36:15 2022
+--
+-- Text encoding used: System
+--
+-- Results of query:
+-- select Clients.Name, Cars.Manufacturer, Cars.Model
+--  from Clients
+--  JOIN Cars
+--  on Clients.id = Cars.id_client
+--  where Birth_year < 2005
+-- order by Birth_year
+-- LIMIT 3
+--
+BEGIN TRANSACTION;
+INSERT INTO "9_Early_adopters" (Name, Manufacturer, Model) VALUES ('Anatoliy', 'Polestar', 'Three');
+INSERT INTO "9_Early_adopters" (Name, Manufacturer, Model) VALUES ('Tania', 'Nissan', 'Leaf');
+INSERT INTO "9_Early_adopters" (Name, Manufacturer, Model) VALUES ('Victor', 'Audi', 'Etron ');
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
